8423b52e5652bf589e15187ba653407a48811bda
Jaakko Juvonen <jaakko.juvonen@wunderdog.fi>
2017-04-28 19:43:02 +0300

    A bit styles. Initial audio

--- src/Moment/Moment.js
+++ src/Moment/Moment.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './Moment.css'
+
 class Moment extends React.Component {
   static propTypes = {
     moment: PropTypes.object.isRequired
   }
 
-  render() {``
+  render() {
     const { fields } = this.props.moment
 
     const renderImages = () => {
       const image = fields.images[0]
       if (!image) return null
-      return <img src={image.fields.file.url}></img>
+      return <div className="Moment--image" style={{backgroundImage: `url(${image.fields.file.url})`}} />
+    }
+
+    const renderAudio = () => {
+      const audio = fields.sounds[0]
+      if (!audio) return null
+      return <audio src={audio.fields.file.url}>Your browser doesn't support HTML5 audio</audio>
     }
 
     return (
-      <div>
-        <h1>fields.name</h1>
+      <div className="Moment">
+        <h1 className="Moment--title">{fields.name}</h1>
+        {renderAudio()}
         {renderImages()}
       </div>
     )
